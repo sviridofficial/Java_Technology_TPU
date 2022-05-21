63bb8f010521dd1fdac849d4d7c3b822325b49f7
Sviridov Konstantin <kostya.sviridov.01@mail.ru>
2022-05-21 23:56:52 +0700

    Добавил 7 лабораторную работу

--- src/com/company/lab7/Main.java
+++ src/com/company/lab7/Main.java
@@ -1,12 +1,16 @@
 package com.company.lab7;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
+    public static Iterator<Room> iterator;
+    public static int squareBathroom = 0;
+    public static int squareLivingRoom = 0;
+    public static int squareBedroom = 0;
+    public static int squareKitchenRoom = 0;
+
     public static void main(String[] args) {
-        Room[] floorFirst = new Room[20];
+        Room[] floorFirst = new Room[7];
         ArrayList<Room> floorSecond = new ArrayList<>();
         LinkedList<Room> floorThird = new LinkedList<>();
 
@@ -14,8 +18,61 @@ public class Main {
             floorFirst[i] = new Room(getRandomRoomType(), getRandomSquare());
             floorSecond.add(new Room(getRandomRoomType(), getRandomSquare()));
             floorThird.add(new Room(getRandomRoomType(), getRandomSquare()));
+            System.out.println(floorFirst[i].toString() + " Первый этаж.");
+            System.out.println(floorSecond.get(i).toString() + " Второй этаж.");
+            System.out.println(floorThird.get(i).toString() + " Третий этаж.");
         }
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println(getBuildingSumSquare(floorFirst, floorSecond, floorThird));
 
+    }
+
+    public static String getBuildingSumSquare(Room[] floorFirst, ArrayList<Room> floorSecond, LinkedList<Room> floorThird) {
+        int squareSum = 0;
+        Room room;
+        int squareFirstFloor = 0;
+        int squareSecondFloor = 0;
+        int squareThirdFloor = 0;
+
+
+        iterator = Arrays.stream(floorFirst).iterator();
+        while (iterator.hasNext()) {
+            room = iterator.next();
+            squareFirstFloor += room.getSquare();
+            sumSquareRoomType(room);
+            squareSum += room.getSquare();
+        }
+        iterator = floorSecond.iterator();
+        while (iterator.hasNext()) {
+            room = iterator.next();
+            squareSecondFloor += room.getSquare();
+            sumSquareRoomType(room);
+            squareSum += room.getSquare();
+        }
+        iterator = floorThird.iterator();
+        while (iterator.hasNext()) {
+            room = iterator.next();
+            squareThirdFloor += room.getSquare();
+            sumSquareRoomType(room);
+            squareSum += room.getSquare();
+        }
+        return "Площадь здания - " + squareSum + ".\nПлощадь первого этажа - " +
+                squareFirstFloor + ".\nПлощадь второго этажа - " + squareSecondFloor + ".\nПлощадь третьего этажа - " + squareThirdFloor +
+                ".\nПлощадь туалетов - "+ squareBathroom+".\nПлощадь спален - "+ squareBedroom+".\nПлощадь гостинных - "+squareLivingRoom+
+                ".\nПлощадь кухонь - "+ squareKitchenRoom+".";
+
+    }
+
+    public static void sumSquareRoomType(Room room) {
+        if (room.getRoomType() == RoomTypes.BATHROOM) {
+            squareBathroom += room.getSquare();
+        } else if (room.getRoomType() == RoomTypes.LIVING_ROOM) {
+            squareLivingRoom += room.getSquare();
+        } else if (room.getRoomType() == RoomTypes.BEDROOM) {
+            squareBedroom += room.getSquare();
+        } else {
+            squareKitchenRoom += room.getSquare();
+        }
     }
 
     public static int getRandomSquare() {
--- src/com/company/lab7/Room.java
+++ src/com/company/lab7/Room.java
@@ -27,6 +27,7 @@ public class Room {
 
     @Override
     public String toString() {
-        return "Room type is " + roomType + ". Square is " + square;
+        return "Room type is " + roomType + ". Square is " + square + ".";
+
     }
 }
